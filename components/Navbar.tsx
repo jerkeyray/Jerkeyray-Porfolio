fce59a07050c9ce13aa219cff5de265e96733ee9
srivastavya02 <srivastavya24@gmail.com>
2025-04-07 09:22:28 +0530

    fixed navbar padding

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,10 +7,10 @@ interface NavbarProps {
 
 const Navbar = ({ currentPage = "home" }: NavbarProps) => {
   return (
-    <nav className="w-full bg-transparent px-4 sm:px-0">
+    <nav className="w-full bg-transparent px-4 sm:px-6 md:px-8">
       <div className="max-w-7xl mx-auto">
         <div className="flex justify-end items-center h-16 sm:h-12">
-          <div className="flex space-x-4 sm:space-x-8">
+          <div className="flex space-x-4 sm:space-x-8 pr-2 sm:pr-4 md:pr-6">
             {currentPage !== "home" && (
               <Link
                 href="/"
