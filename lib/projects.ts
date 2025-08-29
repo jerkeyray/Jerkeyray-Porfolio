b4f0c34e1c7fcefe593756a3318b97b178426c27
srivastavya02 <srivastavya24@gmail.com>
2025-08-29 07:24:15 +0530

    feat: add gostore to projects

--- lib/projects.ts
+++ lib/projects.ts
@@ -1,7 +1,7 @@
 export const projects = [
   {
     title: "Yudoku",
-    description: "The YouTube Course Experience Platform",
+    description: "the youtube course experience platform",
     tech: ["Next.js", "Supabase", "Prisma", "Tailwind CSS", "PostgreSQL"],
     image: "/project-images/interviewinator.png",
     liveUrl: "https://yudoku.jerkeyray.com",
@@ -9,11 +9,18 @@ export const projects = [
   },
   {
     title: "Hookinator",
-    description: "Webhook Toolkit for Developers",
+    description: "webhook woolkit for developers",
     tech: ["Next.js", "Go", "Tailwind CSS", "PostgreSQL"],
     image: "/project-images/hookinator.png",
     liveUrl: "https://hookinator.jerkeyray.com",
     githubUrl: "https://github.com/jerkeyray/hookinator.git",
   },
+  {
+    title: "GoStore",
+    description: "redis like key-value store from scratch in go",
+    tech: ["Go"],
+    image: "/project-images/hookinator.png",
+    githubUrl: "https://github.com/jerkeyray/gostore.git",
+  }
 ];
 
